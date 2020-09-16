22bf48a3fd1be437c7e03125a0dcdbb7cd9d7941
Brian Webb <BrianMinister@users.noreply.github.com>
2020-09-16 18:46:12 -0500

    1.1.0 - Rename ToReturn<> to ToReturnAsync<>; Add ToReturn<> that is not Task Based.

--- TheGoodReturnModel/ToReturnExtension.cs
+++ TheGoodReturnModel/ToReturnExtension.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Dynamic;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace TheGoodReturnModel
@@ -17,7 +16,7 @@ namespace TheGoodReturnModel
         /// <param name="FailMessage">The fail message.</param>
         /// <param name="CancelledMessage">The cancelled message.</param>
         /// <returns></returns>
-        public static async Task<Return<Tout>> ToReturn<Tout>(
+        public static async Task<Return<Tout>> ToReturnAsync<Tout>(
             this Task<Tout> task,
             string SuccessMessage,
             string FailMessage,
@@ -82,7 +81,31 @@ namespace TheGoodReturnModel
         /// <param name="state">The state.</param>
         /// <param name="message">The message.</param>
         /// <returns></returns>
-        public static async Task<Return<Tout>> ToReturn<Tout>(
+        public static Task<Return<Tout>> ToReturnAsync<Tout>(
+            this Tout value,
+            ReturnState state,
+            string message)
+        {
+            dynamic meta = new ExpandoObject();
+            meta.message = message;
+            Return<Tout> Cret = new Return<Tout>()
+            {
+                ReturnData = value,
+                Status = state,
+                Metadata = meta
+            };
+            return Task.FromResult(Cret);
+        }
+
+        /// <summary>
+        /// Converts to return.
+        /// </summary>
+        /// <typeparam name="Tout">The type of the out.</typeparam>
+        /// <param name="value">The value.</param>
+        /// <param name="state">The state.</param>
+        /// <param name="message">The message.</param>
+        /// <returns></returns>
+        public static Return<Tout> ToReturn<Tout>(
             this Tout value,
             ReturnState state,
             string message)
@@ -96,6 +119,438 @@ namespace TheGoodReturnModel
                 Metadata = meta
             };
             return Cret;
+        }
+
+        public static Return<Tout> ToReturn<Tout>(
+            this Func<Tout> call,
+            string SuccessMessage,
+            string FailMessage)
+        {
+            Return<Tout> ret;
+            dynamic meta = new ExpandoObject();
+            try
+            {
+                meta.message = SuccessMessage;
+                ret = new Return<Tout>()
+                {
+                    ReturnData = call.Invoke(),
+                    Status = ReturnState.Success,
+                    Metadata = meta
+                };
+            }
+            catch (Exception e)
+            {
+                meta.message = FailMessage;
+                ret = new Return<Tout>()
+                {
+                    ReturnData = Activator.CreateInstance<Tout>(),
+                    Status = ReturnState.Failed,
+                    Metadata = meta
+                };
+            }
+            return ret;
+        }
+
+        public static Return<Tout> ToReturn<Tin, Tout>(
+            this Func<Tin, Tout> call,
+            Tin Val1,
+            string SuccessMessage,
+            string FailMessage)
+        {
+            Return<Tout> ret;
+            dynamic meta = new ExpandoObject();
+            try
+            {
+                meta.message = SuccessMessage;
+                ret = new Return<Tout>()
+                {
+                    ReturnData = call.Invoke(Val1),
+                    Status = ReturnState.Success,
+                    Metadata = meta
+                };
+            }
+            catch (Exception e)
+            {
+                meta.message = FailMessage;
+                ret = new Return<Tout>()
+                {
+                    ReturnData = Activator.CreateInstance<Tout>(),
+                    Status = ReturnState.Failed,
+                    Metadata = meta
+                };
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// Converts to return.
+        /// </summary>
+        /// <typeparam name="Tin1">The type of the in1.</typeparam>
+        /// <typeparam name="Tin2">The type of the in2.</typeparam>
+        /// <typeparam name="Tout">The type of the out.</typeparam>
+        /// <param name="call">The call.</param>
+        /// <param name="Val1">The val1.</param>
+        /// <param name="Val2">The val2.</param>
+        /// <param name="SuccessMessage">The success message.</param>
+        /// <param name="FailMessage">The fail message.</param>
+        /// <returns></returns>
+        public static Return<Tout> ToReturn<Tin1, Tin2, Tout>(
+            this Func<Tin1, Tin2, Tout> call,
+            Tin1 Val1,
+            Tin2 Val2,
+            string SuccessMessage,
+            string FailMessage)
+        {
+            Return<Tout> ret;
+            dynamic meta = new ExpandoObject();
+            try
+            {
+                meta.message = SuccessMessage;
+                ret = new Return<Tout>()
+                {
+                    ReturnData = call.Invoke(Val1, Val2),
+                    Status = ReturnState.Success,
+                    Metadata = meta
+                };
+            }
+            catch (Exception e)
+            {
+                meta.message = FailMessage;
+                ret = new Return<Tout>()
+                {
+                    ReturnData = Activator.CreateInstance<Tout>(),
+                    Status = ReturnState.Failed,
+                    Metadata = meta
+                };
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// Converts to return.
+        /// </summary>
+        /// <typeparam name="Tin1">The type of the in1.</typeparam>
+        /// <typeparam name="Tin2">The type of the in2.</typeparam>
+        /// <typeparam name="Tin3">The type of the in3.</typeparam>
+        /// <typeparam name="Tout">The type of the out.</typeparam>
+        /// <param name="call">The call.</param>
+        /// <param name="Val1">The val1.</param>
+        /// <param name="Val2">The val2.</param>
+        /// <param name="Val3">The val3.</param>
+        /// <param name="SuccessMessage">The success message.</param>
+        /// <param name="FailMessage">The fail message.</param>
+        /// <returns></returns>
+        public static Return<Tout> ToReturn<Tin1, Tin2, Tin3, Tout>(
+            this Func<Tin1, Tin2, Tin3, Tout> call,
+            Tin1 Val1,
+            Tin2 Val2,
+            Tin3 Val3,
+            string SuccessMessage,
+            string FailMessage)
+        {
+            Return<Tout> ret;
+            dynamic meta = new ExpandoObject();
+            try
+            {
+                meta.message = SuccessMessage;
+                ret = new Return<Tout>()
+                {
+                    ReturnData = call.Invoke(Val1, Val2, Val3),
+                    Status = ReturnState.Success,
+                    Metadata = meta
+                };
+            }
+            catch (Exception e)
+            {
+                meta.message = FailMessage;
+                ret = new Return<Tout>()
+                {
+                    ReturnData = Activator.CreateInstance<Tout>(),
+                    Status = ReturnState.Failed,
+                    Metadata = meta
+                };
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// Converts to return.
+        /// </summary>
+        /// <typeparam name="Tin1">The type of the in1.</typeparam>
+        /// <typeparam name="Tin2">The type of the in2.</typeparam>
+        /// <typeparam name="Tin3">The type of the in3.</typeparam>
+        /// <typeparam name="Tin4">The type of the in4.</typeparam>
+        /// <typeparam name="Tout">The type of the out.</typeparam>
+        /// <param name="call">The call.</param>
+        /// <param name="Val1">The val1.</param>
+        /// <param name="Val2">The val2.</param>
+        /// <param name="Val3">The val3.</param>
+        /// <param name="Val4">The val4.</param>
+        /// <param name="SuccessMessage">The success message.</param>
+        /// <param name="FailMessage">The fail message.</param>
+        /// <returns></returns>
+        public static Return<Tout> ToReturn<Tin1, Tin2, Tin3, Tin4, Tout>(
+            this Func<Tin1, Tin2, Tin3, Tin4, Tout> call,
+            Tin1 Val1,
+            Tin2 Val2,
+            Tin3 Val3,
+            Tin4 Val4,
+            string SuccessMessage,
+            string FailMessage)
+        {
+            Return<Tout> ret;
+            dynamic meta = new ExpandoObject();
+            try
+            {
+                meta.message = SuccessMessage;
+                ret = new Return<Tout>()
+                {
+                    ReturnData = call.Invoke(Val1, Val2, Val3, Val4),
+                    Status = ReturnState.Success,
+                    Metadata = meta
+                };
+            }
+            catch (Exception e)
+            {
+                meta.message = FailMessage;
+                ret = new Return<Tout>()
+                {
+                    ReturnData = Activator.CreateInstance<Tout>(),
+                    Status = ReturnState.Failed,
+                    Metadata = meta
+                };
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// Converts to return.
+        /// </summary>
+        /// <typeparam name="Tin1">The type of the in1.</typeparam>
+        /// <typeparam name="Tin2">The type of the in2.</typeparam>
+        /// <typeparam name="Tin3">The type of the in3.</typeparam>
+        /// <typeparam name="Tin4">The type of the in4.</typeparam>
+        /// <typeparam name="Tin5">The type of the in5.</typeparam>
+        /// <typeparam name="Tout">The type of the out.</typeparam>
+        /// <param name="call">The call.</param>
+        /// <param name="Val1">The val1.</param>
+        /// <param name="Val2">The val2.</param>
+        /// <param name="Val3">The val3.</param>
+        /// <param name="Val4">The val4.</param>
+        /// <param name="Val5">The val5.</param>
+        /// <param name="SuccessMessage">The success message.</param>
+        /// <param name="FailMessage">The fail message.</param>
+        /// <returns></returns>
+        public static Return<Tout> ToReturn<Tin1, Tin2, Tin3, Tin4, Tin5, Tout>(
+            this Func<Tin1, Tin2, Tin3, Tin4, Tin5, Tout> call,
+            Tin1 Val1,
+            Tin2 Val2,
+            Tin3 Val3,
+            Tin4 Val4,
+            Tin5 Val5,
+            string SuccessMessage,
+            string FailMessage)
+        {
+            Return<Tout> ret;
+            dynamic meta = new ExpandoObject();
+            try
+            {
+                meta.message = SuccessMessage;
+                ret = new Return<Tout>()
+                {
+                    ReturnData = call.Invoke(Val1, Val2, Val3, Val4, Val5),
+                    Status = ReturnState.Success,
+                    Metadata = meta
+                };
+            }
+            catch (Exception e)
+            {
+                meta.message = FailMessage;
+                ret = new Return<Tout>()
+                {
+                    ReturnData = Activator.CreateInstance<Tout>(),
+                    Status = ReturnState.Failed,
+                    Metadata = meta
+                };
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// Converts to return.
+        /// </summary>
+        /// <typeparam name="Tin1">The type of the in1.</typeparam>
+        /// <typeparam name="Tin2">The type of the in2.</typeparam>
+        /// <typeparam name="Tin3">The type of the in3.</typeparam>
+        /// <typeparam name="Tin4">The type of the in4.</typeparam>
+        /// <typeparam name="Tin5">The type of the in5.</typeparam>
+        /// <typeparam name="Tin6">The type of the in6.</typeparam>
+        /// <typeparam name="Tout">The type of the out.</typeparam>
+        /// <param name="call">The call.</param>
+        /// <param name="Val1">The val1.</param>
+        /// <param name="Val2">The val2.</param>
+        /// <param name="Val3">The val3.</param>
+        /// <param name="Val4">The val4.</param>
+        /// <param name="Val5">The val5.</param>
+        /// <param name="Val6">The val6.</param>
+        /// <param name="SuccessMessage">The success message.</param>
+        /// <param name="FailMessage">The fail message.</param>
+        /// <returns></returns>
+        public static Return<Tout> ToReturn<Tin1, Tin2, Tin3, Tin4, Tin5, Tin6, Tout>(
+            this Func<Tin1, Tin2, Tin3, Tin4, Tin5, Tin6, Tout> call,
+            Tin1 Val1,
+            Tin2 Val2,
+            Tin3 Val3,
+            Tin4 Val4,
+            Tin5 Val5,
+            Tin6 Val6,
+            string SuccessMessage,
+            string FailMessage)
+        {
+            Return<Tout> ret;
+            dynamic meta = new ExpandoObject();
+            try
+            {
+                meta.message = SuccessMessage;
+                ret = new Return<Tout>()
+                {
+                    ReturnData = call.Invoke(Val1, Val2, Val3, Val4, Val5, Val6),
+                    Status = ReturnState.Success,
+                    Metadata = meta
+                };
+            }
+            catch (Exception e)
+            {
+                meta.message = FailMessage;
+                ret = new Return<Tout>()
+                {
+                    ReturnData = Activator.CreateInstance<Tout>(),
+                    Status = ReturnState.Failed,
+                    Metadata = meta
+                };
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// Converts to return.
+        /// </summary>
+        /// <typeparam name="Tin1">The type of the in1.</typeparam>
+        /// <typeparam name="Tin2">The type of the in2.</typeparam>
+        /// <typeparam name="Tin3">The type of the in3.</typeparam>
+        /// <typeparam name="Tin4">The type of the in4.</typeparam>
+        /// <typeparam name="Tin5">The type of the in5.</typeparam>
+        /// <typeparam name="Tin6">The type of the in6.</typeparam>
+        /// <typeparam name="Tin7">The type of the in7.</typeparam>
+        /// <typeparam name="Tout">The type of the out.</typeparam>
+        /// <param name="call">The call.</param>
+        /// <param name="Val1">The val1.</param>
+        /// <param name="Val2">The val2.</param>
+        /// <param name="Val3">The val3.</param>
+        /// <param name="Val4">The val4.</param>
+        /// <param name="Val5">The val5.</param>
+        /// <param name="Val6">The val6.</param>
+        /// <param name="Val7">The val7.</param>
+        /// <param name="SuccessMessage">The success message.</param>
+        /// <param name="FailMessage">The fail message.</param>
+        /// <returns></returns>
+        public static Return<Tout> ToReturn<Tin1, Tin2, Tin3, Tin4, Tin5, Tin6, Tin7, Tout>(
+            this Func<Tin1, Tin2, Tin3, Tin4, Tin5, Tin6, Tin7, Tout> call,
+            Tin1 Val1,
+            Tin2 Val2,
+            Tin3 Val3,
+            Tin4 Val4,
+            Tin5 Val5,
+            Tin6 Val6,
+            Tin7 Val7,
+            string SuccessMessage,
+            string FailMessage)
+        {
+            Return<Tout> ret;
+            dynamic meta = new ExpandoObject();
+            try
+            {
+                meta.message = SuccessMessage;
+                ret = new Return<Tout>()
+                {
+                    ReturnData = call.Invoke(Val1, Val2, Val3, Val4, Val5, Val6, Val7),
+                    Status = ReturnState.Success,
+                    Metadata = meta
+                };
+            }
+            catch (Exception e)
+            {
+                meta.message = FailMessage;
+                ret = new Return<Tout>()
+                {
+                    ReturnData = Activator.CreateInstance<Tout>(),
+                    Status = ReturnState.Failed,
+                    Metadata = meta
+                };
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// Converts to return.
+        /// </summary>
+        /// <typeparam name="Tin1">The type of the in1.</typeparam>
+        /// <typeparam name="Tin2">The type of the in2.</typeparam>
+        /// <typeparam name="Tin3">The type of the in3.</typeparam>
+        /// <typeparam name="Tin4">The type of the in4.</typeparam>
+        /// <typeparam name="Tin5">The type of the in5.</typeparam>
+        /// <typeparam name="Tin6">The type of the in6.</typeparam>
+        /// <typeparam name="Tin7">The type of the in7.</typeparam>
+        /// <typeparam name="Tin8">The type of the in8.</typeparam>
+        /// <typeparam name="Tout">The type of the out.</typeparam>
+        /// <param name="call">The call.</param>
+        /// <param name="Val1">The val1.</param>
+        /// <param name="Val2">The val2.</param>
+        /// <param name="Val3">The val3.</param>
+        /// <param name="Val4">The val4.</param>
+        /// <param name="Val5">The val5.</param>
+        /// <param name="Val6">The val6.</param>
+        /// <param name="Val7">The val7.</param>
+        /// <param name="Val8">The val8.</param>
+        /// <param name="SuccessMessage">The success message.</param>
+        /// <param name="FailMessage">The fail message.</param>
+        /// <returns></returns>
+        public static Return<Tout> ToReturn<Tin1, Tin2, Tin3, Tin4, Tin5, Tin6, Tin7, Tin8, Tout>(
+            this Func<Tin1, Tin2, Tin3, Tin4, Tin5, Tin6, Tin7, Tin8, Tout> call,
+            Tin1 Val1,
+            Tin2 Val2,
+            Tin3 Val3,
+            Tin4 Val4,
+            Tin5 Val5,
+            Tin6 Val6,
+            Tin7 Val7,
+            Tin8 Val8,
+            string SuccessMessage,
+            string FailMessage)
+        {
+            Return<Tout> ret;
+            dynamic meta = new ExpandoObject();
+            try
+            {
+                meta.message = SuccessMessage;
+                ret = new Return<Tout>()
+                {
+                    ReturnData = call.Invoke(Val1, Val2, Val3, Val4, Val5, Val6, Val7, Val8),
+                    Status = ReturnState.Success,
+                    Metadata = meta
+                };
+            }
+            catch (Exception e)
+            {
+                meta.message = FailMessage;
+                ret = new Return<Tout>()
+                {
+                    ReturnData = Activator.CreateInstance<Tout>(),
+                    Status = ReturnState.Failed,
+                    Metadata = meta
+                };
+            }
+            return ret;
         }
 
     }
--- TheGoodReturnModelTests/DemoRunAsyncAndToReturn.cs
+++ TheGoodReturnModelTests/DemoRunAsyncAndToReturn.cs
@@ -16,12 +16,13 @@ namespace TheGoodReturnModelTests
         [Test]
         public async Task Test_ToReturnFromFunction()
         {
-            var t1 = await ((Func<int, int, int>)TestAddReturn).RunAsync(2, 2).ToReturn(ReturnState.Success, "Test");
+            //Async Calls
+            var t1 = await ((Func<int, int, int>)TestAddReturn).RunAsync(2, 2).ToReturnAsync(ReturnState.Success, "Test");
             Assert.AreEqual(await t1.ReturnData, 4);
             Assert.AreEqual(t1.Status, ReturnState.Success);
             Assert.AreEqual(t1.Metadata.message, "Test");
 
-            var t2 = await TestAddReturnAsync(2, 2).ToReturn(ReturnState.Cancelled, "");
+            var t2 = await TestAddReturnAsync(2, 2).ToReturnAsync(ReturnState.Cancelled, "");
             Assert.AreEqual(await t2.ReturnData, 4);
             Assert.AreEqual(t2.Status, ReturnState.Cancelled);
             Assert.AreEqual(t2.Metadata.message, "");
@@ -31,15 +32,38 @@ namespace TheGoodReturnModelTests
             Assert.AreEqual(t3.Status, ReturnState.Failed);
             Assert.AreEqual(t3.Metadata.message, "Burp");
 
-            var t4 = await TestAddReturnAsync(2, 2, true).ToReturn("Success","Fail","Cancel");
+            var t4 = await TestAddReturnAsync(2, 2, true).ToReturnAsync("Success","Fail","Cancel");
             Assert.AreEqual(t4.ReturnData, 4);
             Assert.AreEqual(t4.Status, ReturnState.Success);
             Assert.AreEqual(t4.Metadata.message, "Success");
 
-            var t5 = await TestAddReturnAsync(2, 3, false).ToReturn("Success", "Fail", "Cancel");
+            var t5 = await TestAddReturnAsync(2, 3, false).ToReturnAsync("Success", "Fail", "Cancel");
             Assert.AreNotEqual(t5.ReturnData, 4);
             Assert.AreEqual(t5.Status, ReturnState.Cancelled);
             Assert.AreEqual(t5.Metadata.message, "Cancel");
+
+            //Sync Calls
+            var t6 = TestAddReturn(2,2).ToReturn(ReturnState.Success, "Success");
+            Assert.AreEqual(t6.ReturnData, 4);
+            Assert.AreEqual(t6.Status, ReturnState.Success);
+            Assert.AreEqual(t6.Metadata.message, "Success");
+
+            var t7 = TestAddReturnAsync(2, 2).ToReturn(ReturnState.Cancelled, "Cancelled");
+            Assert.AreEqual(await t7.ReturnData, 4);
+            Assert.AreEqual(t7.Status, ReturnState.Cancelled);
+            Assert.AreEqual(t7.Metadata.message, "Cancelled");
+
+            Func<int, int, bool, int> d1 = TestAddWithException;
+
+            var t8 = d1.ToReturn(2, 2, true,"Success", "Fail");
+            Assert.AreEqual(t8.ReturnData, 4);
+            Assert.AreEqual(t8.Status, ReturnState.Success);
+            Assert.AreEqual(t8.Metadata.message, "Success");
+
+            var t9 = d1.ToReturn(2, 3, false,"Success", "Fail");
+            Assert.AreNotEqual(t9.ReturnData, 4);
+            Assert.AreEqual(t9.Status, ReturnState.Failed);
+            Assert.AreEqual(t9.Metadata.message, "Fail");
         }
 
         public int TestAddReturn(int value1, int value2)
@@ -73,11 +97,28 @@ namespace TheGoodReturnModelTests
             return ret;
         }
 
+        public int TestAddWithException(int value1, int value2, bool isPass)
+        {
+            Func<int, int, int> call = TestAddReturn;
+            var x = call.Invoke(value1, value2);
+            int ret = 0;
+            if (isPass)
+            {
+                ret = x;
+            }
+            else
+            {
+                throw new Exception("This is a fake Exception.");
+            }
+            return ret;
+        }
+
         public async Task<Return<int>> TestAddReturnAsync(int value1, int value2, ReturnState state, string msg)
         {
             var ret = await TestAddReturnAsync(value1, value2);
-            return await ret.ToReturn(state, msg);
+            return await ret.ToReturnAsync(state, msg);
         }
 
     }
+
 }
